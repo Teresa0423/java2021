f71f215dcb08bb21c1796dd59f9ecbd9c51c0b4d
Teresa04123 <teresawang0423@gmail.com>
2021-03-30 16:31:21 +0800

    commit box2

--- src/main/java/fju/com/box/Box3.java
+++ src/main/java/fju/com/box/Box3.java
@@ -2,9 +2,9 @@ package fju.com.box;
 
 public class Box3 extends Box{
     public Box3(){
-        lenght = 10,
+        lenght = 10;
         width = 10;
-        height = 10,
+        height = 10;
     }
     @Override
     public String getName(){
