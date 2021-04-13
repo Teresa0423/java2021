576852f5e3a762ba9b874263c678a382596bdaa1
Teresa0423 <teresawang0423@gmail.com>
2021-04-13 14:18:56 +0800

    commit1 3

--- src/main/java/fju/com/box/Tester.java
+++ src/main/java/fju/com/box/Tester.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Box3 box3 = new Box3();
+        Box5 box5 = new Box5();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter box's lenght: ");
         int lenght = Integer.parseInt(scanner.next());
@@ -14,7 +15,9 @@ public class Tester {
         int height = Integer.parseInt(scanner.next());
         System.out.println("lenght: " + lenght + ",width:" + width + ",height:" + height);
         if (box3.validate(lenght,width,height)){
-            System.out.println(box3.getName() + box3.getPrice());
+            System.out.println(box3.getName() + "\t" + box3.getPrice());
+        }else if(box5.validate(lenght,width,height)){
+            System.out.println(box5.getName() + "\t" + box5.getPrice());
         }
     }
 }
