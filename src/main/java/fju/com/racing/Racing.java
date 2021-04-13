7c30485c994f7951e4db049001f92664330e42bc
Teresa0423 <teresawang0423@gmail.com>
2021-04-13 15:19:09 +0800

    commit1 4

---
+++ src/main/java/fju/com/racing/Racing.java
@@ -0,0 +1,11 @@
+package fju.com.racing;
+
+public class Racing {
+    public static void main(String[] args){
+        String name1 = "Horse1";
+        Horse h1 = new Horse();
+        Horse h2 = new Horse();
+        h1.start();
+        h2.start();
+    }
+}
