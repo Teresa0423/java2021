729cb14ff1363e6ae1ad2367e1cd7ebc1ee3a897
Teresa Wnag <teresawang0423@gmail.com>
2021-10-31 22:22:33 +0800

    commit 7

--- src/main/java/fju/com/homework/BinomialCoefficient.java
+++ src/main/java/fju/com/homework/BinomialCoefficient.java
@@ -1,23 +1,9 @@
 package fju.com.homework;
 
-import java.util.Scanner;
-
 public class BinomialCoefficient {
-    public static long fact(int i) {
-        if(i <= 1) {
+    public static long binom(int k, int n) {
+        if (k == n || k == 0)
             return 1;
-        }
-        return i * fact(i - 1);
-    }
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter n value: ");
-
-        int n = sc.nextInt();
-        System.out.println("Enter r value: ");
-
-        int r = sc.nextInt();
-        long ncr = fact(n)/(fact(r)*fact(n-r));
-        System.out.println("c("+n+", "+r+") :"+ ncr);
+        else return binom(n-1,k-1) + binom(n-1,k);
     }
 }
